e7a1cf946f20730b0048db3dc7f2b92062ea06ff
Marc <marc.bluemner@gmail.com>
2014-09-25 16:00:46 +0200

    facebook login directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -12,9 +12,39 @@ angular.module('myApp.directives', []).
 
   .directive('myFacebook', [function() {
     return{
-      link: function(scope, element, attributes) {
+      link: function(scope, element, attrs) {
+
+        // Looad the SDK asynchronously
+        (function(d) {
+          var js, id = 'facebook-jssdk',
+          ref = d.getElementsByTagName('script')[0];
+          if (d.getElementById(id)) {
+            return;
+          }
+          js = d.createElement('script');
+          js.id = id;
+          js.async = true;
+          js.src = "//connect.facebook.net/en_US/all.js";
+          ref.parentNode.insertBefore(js, ref);
+        }(document));
+
+        // Initialize FB
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId: '651432224964508',
+            status: true, // ccheck login status
+            cookie: true, // enable cookies to access the session
+            xfbml: false // parse XFBML
+          });
+
+          // Check FB Status
+          FB.getLoginStatus(function(response) {
+            console.log(response);
+          });
+        };
         scope.username = "Marc Bluemner"
       },
       template: "Welcome {{ username }}"
-    }
-  }])
+    };
+  }
+  ]);
